9bd31a5d9088cc665a2c41b7fb4f0cf4017ab668
Vaibhav Kaushal <51777156+vaibhav1465@users.noreply.github.com>
2021-09-21 01:31:27 +0530

    Add files via upload

--- Day 1/FirstProgram.java
+++ Day 1/FirstProgram.java
@@ -1,6 +1,16 @@
 public class FirstProgram{
  public static void main(String[] args) {
-        System.out.print("hello world\n");
-        System.out.print("hello world");
+        // String name="vaibhav";
+        // String name1="vaibhav";
+        // if(name==name1)
+        // System.out.print("true");
+        // else
+        // System.out.print("false");
+        // long a=13;
+        // short b=(short)135555;
+        // System.out.print(b);
+        float c=12.65f;
+        int d=(int)c;
+        System.out.print(d);
     }
 }
