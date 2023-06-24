7a4507d5d357ca8ec660f7643fe27cbc70a8a1c8
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2023-06-24 17:49:05 +0530

    Creating a Graph

--- Data_Structures/Graphs/graph_1.java
+++ Data_Structures/Graphs/graph_1.java
@@ -1,1 +1,64 @@
 // Creating a Graph
+
+import java.util.ArrayList;
+class graph_1{
+
+    static class Edge {
+        int dest;
+        int src;
+        int wt;
+        public Edge(int s,int d,int w){
+            this.src  = s;
+            this.dest = d;
+            this.wt   = w;
+        }
+    }
+    
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for(int i=0;i<graph.length;i++){
+            graph[i]=new ArrayList<Edge>();
+       }
+
+       graph[0].add(new Edge(0, 1, 0));
+       graph[0].add(new Edge(0, 2, 0));
+       graph[0].add(new Edge(0, 3, 0));
+       graph[0].add(new Edge(0, 4, 0));
+
+
+       graph[1].add(new Edge(1, 0, 0));
+       graph[1].add(new Edge(1, 3, 0));
+
+
+       graph[2].add(new Edge(2, 0, 0));
+       graph[2].add(new Edge(2, 3, 0));
+       graph[2].add(new Edge(2, 4, 0));
+
+       graph[3].add(new Edge(3, 0, 0));
+       graph[3].add(new Edge(3, 1, 0));
+       graph[3].add(new Edge(3, 2, 0));
+
+       graph[4].add(new Edge(4, 0, 0));
+       graph[4].add(new Edge(4, 2, 0));
+       
+    }
+ 
+
+    public static void printGraph(ArrayList<Edge> graph[]){
+
+        for(int i=0;i<graph.length;i++){
+            for(int j=0;j<graph[i].size();j++){
+                Edge e = graph[i].get(j);
+                int dest = e.dest;
+                int src  = e.src;
+                System.out.println("Edge------------"+src+"---------->"+dest  );
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int V=5;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        printGraph(graph);
+    }
+}
