251ebfd1574d153e813492d1b5b7d1934fc2ee3c
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2023-07-02 02:39:00 +0530

    Knights Stpes

--- PracticeSheet/Graphs/Graph_7.java
+++ PracticeSheet/Graphs/Graph_7.java
@@ -1,102 +1,79 @@
-//{ Driver Code Starts
-    import java.util.*;
-    import java.lang.*;
-    import java.io.*;
-    class GFG
-    {
-        public static void main(String[] args) throws IOException
-        {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            int T = Integer.parseInt(br.readLine().trim());
-            while(T-->0)
-            {
-                int N = Integer.parseInt(br.readLine().trim());
-                String[] S1 = br.readLine().trim().split(" ");
-                String[] S2 = br.readLine().trim().split(" ");
-                int[] KnightPos = new int[2];
-                int[] TargetPos = new int[2];
-                for(int i = 0; i < 2; i++){
-                    KnightPos[i] = Integer.parseInt(S1[i]);
-                    TargetPos[i] = Integer.parseInt(S2[i]);
-                }
+package PracticeSheet.Graphs;
+import java.util.*;
+public class Graph_7 {
+    public static void main(String[] args) {
+        int N = 6;
+                int[] KnightPos = {6,6};
+                int[] TargetPos = {1,1};
                 Solution obj = new Solution();
                 int ans = obj.minStepToReachTarget(KnightPos, TargetPos, N);
                 System.out.println(ans);
-           }
-        }
     }
-    
-    // } Driver Code Ends
-    
-    
-    class Solution
-    {
-        static class Pair {
-            int r;
-            int c;
-            int count;
-            Pair(int i,int j,int c){
-                this.r=i;
-                this.c=j;
-                this.count=c;
+
+static class Solution
+{
+    public boolean vaid(int x,int y,int n){
+        if(x>=0 && y>=0 && x<n && y<n )
+        return true;
+        return false;
+    }
+    public void help(int x,int y,int[][] dist,Queue<int[]> que,int[] TargetPos,int N){
+        int [] p={-2,-2,-1,-1,1,1,2,2};
+        int [] q={-1,1,2,-2,2,-2,1,-1};
+        for(int i=0;i<p.length;i++){
+            // if(vaid(x+p[i],y+q[i],N)){
+            // System.out.println("point added to queue for x and y " + x +"----"+ y +" dist[x][y] "+ dist[x][y]);
+            // System.out.println("---------------------------------"+ (x+p[i])+"----"+(y+q[i])+"---old dist loop " + dist[x+p[i]][y+q[i]]);
+            // }
+            if(vaid(x+p[i],y+q[i],N) && dist[x+p[i]][y+q[i]]>dist[x][y]+1){
+                dist[x+p[i]][y+q[i]]=dist[x][y]+1;
+                int[] points={x+p[i],y+q[i]};
+                // System.out.println("---------------------------------"+points[0]+"----------"+points[1]+"---new dist  " + dist[x+p[i]][y+q[i]]);
+                // System.out.println();
+                que.add(points);
+                if(TargetPos[0]-1==x+p[i] && TargetPos[1]-1==y+q[i]){
+                    // System.out.println("found");
+                    // System.out.println();
+                    // System.out.println();
+                    // System.out.println();
+                    // System.out.println();
+                    return;
+                }
             }
         }
-        //Function to find out minimum steps Knight needs to reach target position.
-        public int minStepToReachTarget(int KnightPos[], int TargetPos[], int n)
-        {
-            // Code 
-            int s_r = KnightPos[1];
-            int s_c = KnightPos[0];
-            int d_r = TargetPos[1];
-            int d_c = TargetPos[0];
-           
-            boolean[][] vis = new boolean[n+1][n+1];
-            Queue<Pair> q = new LinkedList<Pair>();
-            q.add(new Pair(s_r,s_c,0));
-            int min_count = Integer.MAX_VALUE;
-            int found=0;
-            
-            while(!q.isEmpty()){
-                 Pair curr = q.remove();
-                 int i = curr.r;
-                 int j = curr.c;
-                 int count = curr.count;
-                 if(i==d_r && j==d_c && min_count > count ){
-                     min_count =count;
-                     found=1;
-                 }
-            if (i + 1 < n+1 && j-2 >=1 && !vis[i + 1][j-2]) {
-                q.add(i-1,j-2,count+1);
-             }
-             if (i + 1 <n+1  && j+2 <n+1 && !vis[i + 1][j+2]) {
-                q.add(i-1,j-2,count+1);
-             }
-             if (i-2 >= 1 && j+1 < n+1 && !vis[i - 2][j+1]) {
-                q.add(i-2,j+1,count+1);
-             }
-             if (i+2 < n+1 && j+1< n+1 && !vis[i + 2 ][j+1]) {
-                q.add(i+2,j+1,count+1);
-             }
-             if (i - 1 >=1  && j-2 >=1 && !vis[i - 1][j-2]) {
-                q.add(i-1,j-2,count +1);
-             }
-             if (i - 1 >=1  && j+2 <n+1 && !vis[i - 1][j+2]) {
-                q.add(i-1,j+2,count + 1);
-             }
-             if (i + 2 < n+1 && j-1 >=1 && !vis[i +2 ][j-1]) {
-                q.add(i+2,j-1,count +1);
-             }
-             if (i - 2 >= 1 && j-1>=1 && !vis[i - 1][j-1]) {
-                q.add(i-2,j-1,count +1 );
-             }
-            }
-            
-            
-            if(found==1)
-            return min_count;
-            else
-            return -1;
-        }
-    }
         
-    
+    }
+    //Function to find out minimum steps Knight needs to reach target position.
+    public int minStepToReachTarget(int KnightPos[], int TargetPos[], int N)
+    {
+        // Code here
+        if(KnightPos[0]==TargetPos[0] && KnightPos[1]==TargetPos[1])
+        return 0;
+        if(KnightPos[0]>N || TargetPos[0]>N || 
+        KnightPos[1]>N || TargetPos[1]>N || 
+        KnightPos[0]<1 || TargetPos[0]<1 || 
+        KnightPos[1]<1 || TargetPos[1]<1)
+        return -1;
+        
+         int[][] dist=new int[N][N];
+         Queue<int[]> que=new LinkedList<>();
+         for(int i=0;i<N;i++){
+             for(int j=0;j<N;j++){
+                 dist[i][j]=Integer.MAX_VALUE;
+             }
+         }
+         dist[KnightPos[0]-1][KnightPos[1]-1]=0;
+         int[] p={KnightPos[0]-1,KnightPos[1]-1};
+         
+         que.add(p);
+         while(!que.isEmpty()){
+             int[] point=que.poll();
+             int x=point[0];
+             int y=point[1];
+             help(x,y,dist,que,TargetPos,N);
+             
+         }
+         return dist[TargetPos[0]-1][TargetPos[1]-1];
+    }
+}
+}
