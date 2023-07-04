164463379acfc2332a9d2824103a99e098316750
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2023-07-04 19:11:29 +0530

    pattern practice

--- Data_Structures/Patterns/pattern_3.java
+++ Data_Structures/Patterns/pattern_3.java
@@ -2,19 +2,29 @@ package Data_Structures.Patterns;
 
 public class pattern_3 {
     public static void main(String[] args) {
-        int n=4;
-        int m=5;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(i==0 || i==n-1)
-                System.out.print("* ");
-                else if(j==0 || j==m-1)
-                System.out.print("* ");
-                else{
-                    System.out.print("  ");
+        int n=10;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(j<=i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
                 }
             }
             System.out.println();
         }
-    }
 }
+
+
+}
+
+
+// *        
+// **       
+// ***      
+// ****     
+// *****    
+// ******   
+// *******  
+// ******** 
+// *********
