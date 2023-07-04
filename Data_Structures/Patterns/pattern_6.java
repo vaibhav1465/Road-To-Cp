830a2ebea4c81c7699d6f9c035492490ca1c4968
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2023-07-04 19:27:39 +0530

    pattern practice

---
+++ Data_Structures/Patterns/pattern_6.java
@@ -0,0 +1,18 @@
+package Data_Structures.Patterns;
+
+public class pattern_6 {
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(j<=i){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+}
+
+}
