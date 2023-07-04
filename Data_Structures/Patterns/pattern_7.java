3fe86fc8097319ca060ea5de6889749dde0d101b
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2023-07-04 19:35:52 +0530

    pattern practice

--- Data_Structures/Patterns/pattern_7.java
+++ Data_Structures/Patterns/pattern_7.java
@@ -1,9 +1,9 @@
 package Data_Structures.Patterns;
 
-public class pattern_6 {
+public class pattern_7 {
     public static void main(String[] args) {
         int n=10;
-        for(int i=1;i<=n;i++){
+        for(int i=n;i>=1;i--){
             for(int j=1;j<=i;j++){
                     System.out.print(j+" ");
                 
@@ -11,16 +11,15 @@ public class pattern_6 {
             System.out.println();
         }
 }
-
 }
 
-// 1
-// 12
-// 123
-// 1234
-// 12345
-// 123456
-// 1234567
-// 12345678
-// 123456789
-// 12345678910
+// 1 2 3 4 5 6 7 8 9 10 
+// 1 2 3 4 5 6 7 8 9 
+// 1 2 3 4 5 6 7 8 
+// 1 2 3 4 5 6 7 
+// 1 2 3 4 5 6 
+// 1 2 3 4 5 
+// 1 2 3 4 
+// 1 2 3 
+// 1 2 
+// 1 
