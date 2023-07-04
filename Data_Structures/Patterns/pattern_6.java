c43e2fb7ce3211bc33500893abe5f05787dd703a
Vaibhav Kaushal <kaushalvaibhav51@gmail.com>
2023-07-04 19:29:41 +0530

    pattern practice

--- Data_Structures/Patterns/pattern_6.java
+++ Data_Structures/Patterns/pattern_6.java
@@ -3,13 +3,10 @@ package Data_Structures.Patterns;
 public class pattern_6 {
     public static void main(String[] args) {
         int n=10;
-        for(int i=1;i<n;i++){
-            for(int j=1;j<n;j++){
-                if(j<=i){
-                    System.out.print("*");
-                }else{
-                    System.out.print(" ");
-                }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                    System.out.print(j);
+                
             }
             System.out.println();
         }
